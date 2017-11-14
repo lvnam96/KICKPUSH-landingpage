cd1057c1f8408336aff3403a1ff0c846dc97d678
lvnam96 <lvnam96@gmail.com>
2017-11-14 17:15:29 +0700

    Add optimizers/minifiers for source files and static assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,48 @@
 var gulp = require('gulp'),
-    // pump = require('pump'),
-    rename = require('gulp-rename'),
     srcPath = './src/',
     distPath = './build/',
-    // Files/Paths that need to be watched by gulp
     watchPaths = {
-        html: srcPath + '*.html',
-        // css: [srcPath + 'css/style.css'],
-        sass: srcPath + 'scss/*/*.scss'
-        // js:   [srcPath + 'js/app.js']
+        imgs: srcPath + 'imgs/**',
+        html: srcPath + 'index.html',
+        css: srcPath + 'css/style.css',
+        sass: srcPath + 'scss/**/*.scss',
+        js: srcPath + 'js/index.js'
     },
     bs = require('browser-sync').create();
 
-gulp.task('browser-sync', function (cb) {
-    bs.init({
-        server: {
-            baseDir: './src'
-        }
-    });
-    gulp.watch('src/scss/*.scss', ['sass']);
-    gulp.watch('src/*.html').on('change', bs.reload);
+gulp.task('html', function (cb) {
+    var htmlmin = require('gulp-htmlmin');
+    return gulp.src(watchPaths.html)
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest(distPath));
 });
 
-// gulp.task('sass', function (cb) {
-//     var sass = require('gulp-sass');
-//     pump([
-//         gulp.src(srcPath + 'scss/*.scss'),
-//         sass().on('error', sass.logError),
-//         gulp.dest(srcPath + 'css')
-//     ], cb);
-// });
 gulp.task('sass', function (cb) {
     var sass = require('gulp-sass');
-
-    return gulp.src('src/scss/*.scss')
+    return gulp.src(srcPath + 'scss/*.scss')
         // .pipe(sass.sync().on('error', sass.logError))
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest(srcPath + 'css'))
         .pipe(bs.reload({stream: true}));
+});
+
+gulp.task('css', ['sass'], function (cb) {
+    var postcss = require('gulp-postcss');
+    return gulp.src(watchPaths.css)
+        .pipe(postcss())
+        .pipe(gulp.dest(distPath + 'css'));
+});
+
+gulp.task('img', function(cb) {
+    var imagemin = require('gulp-imagemin');
+    var pngquant = require('imagemin-pngquant');
+    // gulp.task('images', ['hugo'], () => {
+    return gulp.src(watchPaths.img)
+        .pipe(imagemin({
+            progressive: true,
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest(distPath + 'imgs'));
 });
 
 // gulp.task('js', function (cb) {
@@ -45,34 +50,20 @@ gulp.task('sass', function (cb) {
 //         composer = require('gulp-uglify/composer'),
 //         minify = composer(uglifyjs, console),
 //         options = {};
-
 //     return gulp.src(srcPath + 'js/app.js')
 //         .pipe(minify(options))
 //         .pipe(rename({ suffix: '.min' }))
 //         .pipe(gulp.dest(distPath + 'js'));
 // });
 
-gulp.task('css', function (cb) {
-    var postcss = require('gulp-postcss');
-
-    return gulp.src(srcPath + 'css/style.css')
-        .pipe(postcss())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest(distPath + 'css'));
+gulp.task('browser-sync', function (cb) {
+    bs.init({
+        server: {
+            baseDir: srcPath
+        }
+    });
+    gulp.watch(watchPaths.sass, ['sass']);
+    gulp.watch(watchPaths.html).on('change', bs.reload);
 });
 
-// The watch task will be executed upon each file change
-gulp.task('watch', function () {
-    // gulp.watch(watchPaths.js, ['js']);
-    // gulp.watch(watchPaths.css, ['css']);
-    gulp.watch('src/scss/*.scss', ['sass']);
-    gulp.watch('src/scss/*/*.scss', ['sass']);
-    gulp.watch('src/*.html').on('change', bs.reload);
-});
-// gulp.task('watch-sass-only', function() {
-//     gulp.watch(watchPaths.sass, ['sass']);
-// });
-
-// Default task is executed upon execution of gulp
-// gulp.task('start', ['browser-sync', 'watch']);
-gulp.task('default', ['sass', 'css']);
+gulp.task('default', ['img', 'css', 'html']);
